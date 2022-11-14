233bcd15b97c77c5427c432e51fb98b2303165a8
Abdellah Aarab <105945475+abdellahaarab@users.noreply.github.com>
2022-11-14 21:51:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const TeleBot = require('telebot');
-const bot = new TeleBot("5103997793:AAGi-6lFQQ8Pl_srnf8WhAKCshCQbnuh1wI");
+const bot = new TeleBot("YOUR TOKEN");
 console.log("starting...")
 
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
